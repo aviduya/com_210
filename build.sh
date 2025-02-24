70d32f33fde4a94328c8874f3757638a9e2b76f9
aviduya <anferneev@icloud.com>
2025-02-23 18:21:44 -0800

    Adding HW2, edited build script

--- build.sh
+++ build.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
-# Check if a filename was provided
 if [ -z "$1" ]; then
     echo "Usage: ./build.sh <filename>"
     exit 1
 fi
 
-# Extract filename without extension
 FILENAME="${1%.*}"
 
-# Check if file exists
 if [ ! -f "$1" ]; then
     echo "Error: File '$1' not found!"
     exit 1
 fi
 
-# Compile the specified file
 g++ -std=c++17 -Wall -Wextra -o "$FILENAME" "$1"
 
-# Check if compilation was successful
 if [ $? -eq 0 ]; then
     echo "Compilation successful! Running..."
     ./"$FILENAME"
 
-# Delete the executable after running
     rm "$FILENAME"
 
 else
---
+++ hw_2_node_links.cpp
@@ -0,0 +1,66 @@
+#include <__config>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int STEPS = 10;
+
+struct Node {
+    char data;
+    Node* link1 = nullptr;
+    Node* link2 = nullptr;
+    Node* link3 = nullptr;
+};
+
+int main() {
+    srand(time(0));
+
+    Node L, I, V, E;
+
+    L.data = 'L';
+    I.data = 'I';
+    V.data = 'V';
+    E.data = 'E';
+
+    L.link1 = &I;
+    L.link2 = &V;
+    L.link3 = &E;
+
+    I.link1 = &L;
+    I.link2 = &V;
+    I.link3 = &E;
+
+    V.link1 = &L;
+    V.link2 = &I;
+    V.link3 = &E;
+
+    E.link1 = &L;
+    E.link2 = &I;
+    E.link3 = &V;
+
+    Node* head = &L;
+
+    for (;head != nullptr ;) {
+        cout << head->data << " ";
+
+        int choice = rand() % 3;
+        switch (choice) {
+            case 0:
+            head = head->link1;
+            break;
+
+            case 1:
+            head = head->link2;
+            break;
+
+            case 2:
+            head = head->link3;
+            break;
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
