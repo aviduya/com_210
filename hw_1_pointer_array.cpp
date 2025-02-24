3602248884a273e70a8878d6fb69074c4b92e73f
aviduya <anferneev@icloud.com>
2025-02-23 17:21:54 -0800

    adding bash script to compile and run source files

---
+++ hw_1_pointer_array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+const int SIZE = 10;
+
+int ** getRandomInt() {
+    int ** arr = new int*[SIZE];
+    for(int i = 0; i < SIZE; i++) {
+        arr[i] = new int(rand() % 100);
+    }
+    return arr;
+}
+
+void displayInt(int ** pp) {
+    for(int i = 0; i < SIZE; i++) {
+        cout << *pp[i] << " ";
+    }
+    cout << endl;
+}
+
+void deleteInt(int ** pp) {
+    for(int i = 0; i < SIZE; i++) {
+        delete pp[i];
+    }
+    delete[] pp;
+}
+
+int main() {
+    int ** pp = getRandomInt();
+    displayInt(pp);
+    deleteInt(pp);
+    return 0;
+}
